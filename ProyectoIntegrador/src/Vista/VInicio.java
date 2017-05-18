97c917ddb4df245bb95bc528408452d26f8b7535
21609121 <21609121@IvanZhen.localdomain>
2017-05-18 09:09:55 +0200

    Class Vista.Principal changed

--- ProyectoIntegrador/src/Vista/VInicio.java
+++ ProyectoIntegrador/src/Vista/VInicio.java
@@ -15,15 +15,11 @@ public class VInicio extends JFrame {
 
 	protected JPanel contentPane;
 	protected JButton btnEventos;
-	protected JButton btnLineaPedido;
 	protected JButton btnProveedor;
 	protected JButton btnProyectos;
-	protected JButton btnProyectosMaquina;
-	protected JButton btnProyectosUsuarios;
 	protected JButton btnUsuarios;
 	protected JButton btnUsuariosEventos;
 	protected JButton btnPedidos;
-	protected JButton btnMaterialProyecto;
 	protected JButton btnMaterial;
 	protected JButton btnMaquina;
 	
@@ -48,7 +44,7 @@ public class VInicio extends JFrame {
 		
 		setTitle("Inicio");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 400, 370);
+		setBounds(100, 100, 400, 260);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -62,24 +58,16 @@ public class VInicio extends JFrame {
 		btnEventos.setBounds(30, 13, 150, 40);
 		contentPane.add(btnEventos);
 		
-		btnLineaPedido = new JButton("Linea Pedidos");
-		btnLineaPedido.setBounds(30, 65, 150, 40);
-		contentPane.add(btnLineaPedido);
-		
 		btnMaquina = new JButton("Maquina");
-		btnMaquina.setBounds(30, 117, 150, 40);
+		btnMaquina.setBounds(30, 64, 150, 40);
 		contentPane.add(btnMaquina);
 		
 		btnMaterial = new JButton("Material");
-		btnMaterial.setBounds(30, 169, 150, 40);
+		btnMaterial.setBounds(30, 115, 150, 40);
 		contentPane.add(btnMaterial);
 		
-		btnMaterialProyecto = new JButton("Material Proyecto");
-		btnMaterialProyecto.setBounds(30, 221, 150, 40);
-		contentPane.add(btnMaterialProyecto);
-		
 		btnPedidos = new JButton("Pedidos");
-		btnPedidos.setBounds(30, 273, 150, 40);
+		btnPedidos.setBounds(30, 166, 150, 40);
 		contentPane.add(btnPedidos);
 		
 		btnProveedor = new JButton("Proveedor");
@@ -90,20 +78,12 @@ public class VInicio extends JFrame {
 		btnProyectos.setBounds(205, 65, 150, 40);
 		contentPane.add(btnProyectos);
 		
-		btnProyectosMaquina = new JButton("Proyectos Maquina");
-		btnProyectosMaquina.setBounds(205, 117, 150, 40);
-		contentPane.add(btnProyectosMaquina);
-		
-		btnProyectosUsuarios = new JButton("Proyectos Usuarios");
-		btnProyectosUsuarios.setBounds(205, 169, 150, 40);
-		contentPane.add(btnProyectosUsuarios);
-		
 		btnUsuarios = new JButton("Usuarios");
-		btnUsuarios.setBounds(205, 221, 150, 40);
+		btnUsuarios.setBounds(205, 115, 150, 40);
 		contentPane.add(btnUsuarios);
 		
 		btnUsuariosEventos = new JButton("Usuarios Eventos");
-		btnUsuariosEventos.setBounds(205, 273, 150, 40);
+		btnUsuariosEventos.setBounds(205, 166, 150, 40);
 		contentPane.add(btnUsuariosEventos);
 	}
 	
