4432720faf43d0b9e59babcee1cd8189eb2bc0ce
21609121 <21609121@IvanZhen.localdomain>
2017-05-22 12:07:47 +0200

    CEventos updated

--- ProyectoIntegrador/src/Controlador/CEventos.java
+++ ProyectoIntegrador/src/Controlador/CEventos.java
@@ -40,7 +40,6 @@ public class CEventos implements ActionListener, MouseListener {
 		for (Eventos e : eventos) {
 			DefaultTableModel tabla = (DefaultTableModel) vEventos.table.getModel();
 			tabla.addRow(new Object[] {e.getCod_ev(), e.getFecha(), e.getMentor(), e.getCategoria(), e.getDuracion(), e.getLugar()});
-			System.out.println(e.getFecha());
 		}
 	}
 
