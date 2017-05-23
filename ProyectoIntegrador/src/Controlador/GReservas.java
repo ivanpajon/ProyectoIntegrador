9f2f87cf95fb78d3973407fce072a34ab95d9b4d
21609121 <21609121@IvanZhen.localdomain>
2017-05-23 10:36:20 +0200

    GReservas updated

--- ProyectoIntegrador/src/Controlador/GReservas.java
+++ ProyectoIntegrador/src/Controlador/GReservas.java
@@ -17,14 +17,16 @@ private Connection cn;
 	public void consultarEventos(ArrayList<ProyectosMaquina> eventos) {
 		try {
 			Statement st = cn.createStatement();
-			ResultSet rs = st.executeQuery("SELECT * FROM PROYECTOSMAQUINA ORDER BY COD_PR, COD_MA");
+			//ResultSet rs = st.executeQuery("SELECT * FROM PROYECTOSMAQUINA ORDER BY COD_PR, COD_MA");
+			ResultSet rs = st.executeQuery("SELECT DISTINCT nombre, proyectosmaquina.cod_pr, cod_ma, fecha_inicio, fecha_fin FROM proyectos, proyectosmaquina ORDER BY cod_pr, cod_ma");
 			
 			while(rs.next()) {
 				ProyectosMaquina pm = new ProyectosMaquina();
-				pm.setCod_pr(rs.getString(1));
-				pm.setCod_ma(rs.getString(2));
-				pm.setFecha_inicio(rs.getString(3));
-				pm.setFecha_fin(rs.getString(4));
+				pm.setNombre(rs.getString(1));
+				pm.setCod_pr(rs.getString(2));
+				pm.setCod_ma(rs.getString(3));
+				pm.setFecha_inicio(rs.getString(4));
+				pm.setFecha_fin(rs.getString(5));
 				eventos.add(pm);
 			}
 			
