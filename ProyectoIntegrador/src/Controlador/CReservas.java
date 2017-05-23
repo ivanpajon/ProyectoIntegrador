e67492f5072dadcc60b53240e8a473c7f2129d5b
21609121 <21609121@IvanZhen.localdomain>
2017-05-23 09:09:43 +0200

    CReservas created

--- ProyectoIntegrador/src/Controlador/CReservas.java
+++ ProyectoIntegrador/src/Controlador/CReservas.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import Entidades.Eventos;
 import Entidades.ProyectosMaquina;
 import OracleAccess.OracleAccess;
+import Vista.VEventos;
 import Vista.VReservas;
 
 public class CReservas {
@@ -12,5 +13,17 @@ public class CReservas {
 	GReservas gReservas;
 	OracleAccess bbdd;
 	ArrayList<ProyectosMaquina> reservas = new ArrayList<ProyectosMaquina>();
+	
+	public CReservas(VReservas vReservas, OracleAccess bbdd) {
+		this.vReservas = vReservas;
+		this.bbdd = bbdd;
+		this.gReservas = new GEventos(bbdd.getCn());
+		this.gReservas.consultarEventos(reservas);
+		
+		this.vReservas.dateChooser.getJCalendar().setTodayButtonVisible(true);
+		this.vReservas.dateChooser.getJCalendar().setTodayButtonText("Hoy");
+		
+		cargarEventos();
+	}
 
 }
