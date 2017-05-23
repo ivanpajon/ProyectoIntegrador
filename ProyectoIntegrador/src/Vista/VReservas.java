bbd9b67c3aecda5d7c533f4767b7bcf50f4ba152
21609121 <21609121@IvanZhen.localdomain>
2017-05-23 10:35:42 +0200

    VReservas updated

--- ProyectoIntegrador/src/Vista/VReservas.java
+++ ProyectoIntegrador/src/Vista/VReservas.java
@@ -8,6 +8,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import Controlador.CReservas;
+import Entidades.ProyectosMaquina;
+
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JScrollPane;
@@ -15,6 +17,7 @@ import javax.swing.JTable;
 import com.toedter.calendar.JDateChooser;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.JComboBox;
 
 public class VReservas extends JFrame {
 	public JPanel contentPane;
@@ -23,11 +26,12 @@ public class VReservas extends JFrame {
 	public JTextField tfCodigoProyecto;
 	public JDateChooser dateChooserInicio;
 	public JDateChooser dateChooserFin;
+	public JComboBox<String> comboBox;
 
 	public VReservas() {
 		setTitle("Reservas");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 414);
+		setBounds(100, 100, 450, 480);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -36,32 +40,36 @@ public class VReservas extends JFrame {
 		
 		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation(pantalla.width/3, pantalla.height/5);
+		
+		JLabel lblNombreProyecto = new JLabel("Nombre Proyecto");
+		lblNombreProyecto.setBounds(10, 24, 110, 14);
+		contentPane.add(lblNombreProyecto);
 		//System.out.println("Ancho - " + pantalla.width/3);
 		//System.out.println("Alto - " + pantalla.height/5);
 		
 		JLabel lblCodigoProyecto = new JLabel("Código Proyecto");
-		lblCodigoProyecto.setBounds(10, 25, 110, 14);
+		lblCodigoProyecto.setBounds(10, 63, 110, 14);
 		contentPane.add(lblCodigoProyecto);
 		
 		JLabel lblCodigoMaquina = new JLabel("Código Máquina");
-		lblCodigoMaquina.setBounds(10, 60, 110, 14);
+		lblCodigoMaquina.setBounds(10, 98, 110, 14);
 		contentPane.add(lblCodigoMaquina);
 		
 		JLabel lblFechaInicio = new JLabel("Fecha Inicio");
-		lblFechaInicio.setBounds(10, 100, 110, 14);
+		lblFechaInicio.setBounds(10, 138, 110, 14);
 		contentPane.add(lblFechaInicio);
 		
 		JLabel lblFechaFin = new JLabel("Fecha Fin");
-		lblFechaFin.setBounds(10, 140, 110, 14);
+		lblFechaFin.setBounds(10, 178, 110, 14);
 		contentPane.add(lblFechaFin);
 		
 		JLabel lblError = new JLabel("");
 		lblError.setHorizontalAlignment(SwingConstants.CENTER);
-		lblError.setBounds(10, 184, 414, 14);
+		lblError.setBounds(10, 222, 414, 14);
 		contentPane.add(lblError);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 220, 414, 145);
+		scrollPane.setBounds(10, 275, 414, 145);
 		contentPane.add(scrollPane);
 		
 		table = new JTable();
@@ -69,31 +77,35 @@ public class VReservas extends JFrame {
 			new Object[][] {
 			},
 			new String[] {
-				"Código Proyecto", "Código Máquina", "Fecha Inicio", "Fecha Fin"
+				"Nombre", "Código Proyecto", "Código Máquina", "Fecha Inicio", "Fecha Fin"
 			}
 		));
 		scrollPane.setViewportView(table);
 		
 		dateChooserInicio = new JDateChooser();
-		dateChooserInicio.setBounds(145, 94, 145, 20);
+		dateChooserInicio.setBounds(145, 132, 145, 20);
 		contentPane.add(dateChooserInicio);
 		
 		dateChooserFin = new JDateChooser();
-		dateChooserFin.setBounds(145, 134, 145, 20);
+		dateChooserFin.setBounds(145, 172, 145, 20);
 		contentPane.add(dateChooserFin);
 		
 		tfCodigoMaquina = new JTextField();
-		tfCodigoMaquina.setBounds(145, 57, 145, 20);
+		tfCodigoMaquina.setBounds(145, 95, 145, 20);
 		contentPane.add(tfCodigoMaquina);
 		tfCodigoMaquina.setColumns(10);
 		
 		tfCodigoProyecto = new JTextField();
-		tfCodigoProyecto.setBounds(145, 22, 145, 20);
+		tfCodigoProyecto.setBounds(145, 60, 145, 20);
 		contentPane.add(tfCodigoProyecto);
 		tfCodigoProyecto.setColumns(10);
+		
+		comboBox = new JComboBox<String>();
+		comboBox.setBounds(145, 21, 145, 20);
+		contentPane.add(comboBox);
 	}
 	
 	public void setControlador(CReservas c) {
-		
+		table.addMouseListener(c);
 	}
 }
