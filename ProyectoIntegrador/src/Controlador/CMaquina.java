53e5555d9ba8b614168d0d593c6814030c92e1a8
Ivan Zhen <ivangpajon@hotmail.com>
2017-06-05 16:48:14 +0200

    CMaquina updated

--- ProyectoIntegrador/src/Controlador/CMaquina.java
+++ ProyectoIntegrador/src/Controlador/CMaquina.java
@@ -1,5 +1,6 @@
 package Controlador;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -53,10 +54,83 @@ public class CMaquina implements ActionListener, MouseListener {
 			}
 			else {
 				vMaquina.btnInsertar.setText("Insertar");
-				//insertarMaquina();
+				insertarMaquina();
 				limpiar();
 			}
 		}
+		else if (obj == vMaquina.btnModificar) {
+			modificarMaquina();
+		}
+		else if (obj == vMaquina.btnBorrar) {
+			borrarMaquina();
+		}
+	}
+
+	private void borrarMaquina() {
+		int fila = vMaquina.table.getSelectedRow();
+		
+		maquinas.remove(fila);
+		DefaultTableModel tabla = (DefaultTableModel) vMaquina.table.getModel();
+		tabla.removeRow(fila);
+		gMaquina.borrarMaquina(vMaquina.tfCodigo.getText());
+		
+		this.vMaquina.btnBorrar.setEnabled(false);
+		this.vMaquina.btnModificar.setEnabled(false);
+		limpiar();
+	}
+
+	private void modificarMaquina() {
+		Maquina m = new Maquina();
+		m.setCod_ma(vMaquina.tfCodigo.getText());
+		m.setNombre(vMaquina.tfNombre.getText());
+		m.setDes(vMaquina.tfDescripcion.getText());
+		m.setTipo(vMaquina.tfTipo.getText());
+		
+		if (gMaquina.modificarMaquina(m)) {
+			DefaultTableModel tabla = (DefaultTableModel) vMaquina.table.getModel();
+			int fila = vMaquina.table.getSelectedRow();
+			
+			maquinas.set(fila, m);
+			tabla.setValueAt(m.getCod_ma(), fila, 0);
+			tabla.setValueAt(m.getNombre(), fila, 1);
+			tabla.setValueAt(m.getDes(), fila, 2);
+			tabla.setValueAt(m.getTipo(), fila, 3);
+			
+			vMaquina.lblError.setForeground(Color.GREEN.darker());
+			vMaquina.lblError.setText("La máquina se ha modificado correctamente");
+		}
+		else {
+			vMaquina.lblError.setForeground(Color.RED);
+			vMaquina.lblError.setText("Error modificando la máquina");
+		}
+	}
+
+	private void insertarMaquina() {
+		Maquina m = new Maquina();
+		m.setCod_ma(vMaquina.tfCodigo.getText());
+		m.setNombre(vMaquina.tfNombre.getText());
+		m.setDes(vMaquina.tfDescripcion.getText());
+		m.setTipo(vMaquina.tfTipo.getText());
+		
+		if (vMaquina.tfCodigo.getText().equals("") || vMaquina.tfNombre.getText().equals("") || vMaquina.tfDescripcion.getText().equals("") || vMaquina.tfTipo.getText().equals("")) {
+			vMaquina.lblError.setForeground(Color.RED);
+			vMaquina.lblError.setText("Rellene todos los campos por favor");
+		}
+		else {
+			if (gMaquina.añadirMaquina(m)) {
+				DefaultTableModel tabla = (DefaultTableModel) vMaquina.table.getModel();
+				tabla.addRow(new Object[] {m.getCod_ma(), m.getNombre(), m.getDes(), m.getTipo()});
+				maquinas.add(m);
+				
+				vMaquina.lblError.setForeground(Color.GREEN.darker());
+				vMaquina.lblError.setText("Maquina añadida correctamente");
+			}
+			else {
+				vMaquina.lblError.setForeground(Color.RED);
+				vMaquina.lblError.setText("Ocurrió un error al guardar la maquina");
+			}
+		}
+		
 	}
 
 	private void limpiar() {
