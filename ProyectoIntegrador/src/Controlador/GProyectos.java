ba6a99f900c092448560d53a7d38e6f301c1c8f8
Ivan Zhen <ivangpajon@hotmail.com>
2017-06-12 16:42:47 +0200

    GProyectos updated

--- ProyectoIntegrador/src/Controlador/GProyectos.java
+++ ProyectoIntegrador/src/Controlador/GProyectos.java
@@ -34,4 +34,56 @@ public class GProyectos {
 			System.out.println("Error al consultar los proyectos - " + e);
 		}
 	}
+	
+	public boolean añadirProyecto(Proyectos p) {
+		Statement st;
+		try {
+			st = cn.createStatement();
+			String insert = "INSERT INTO PROYECTOS VALUES('"+p.getCod_pr()+"', '"+p.getNombre()+"', '"+p.getDes()+"')";
+			//System.out.println(insert);
+			
+			st.executeUpdate(insert);
+			st.close();
+			return true;
+		}
+		catch(Exception ex) {
+			System.out.println("Error añadiendo el proyecto - " + ex);
+			return false;
+		}
+	}
+	
+	public boolean modificarProyecto(Proyectos p) {
+		Statement st;
+		try {
+			st = cn.createStatement();
+			String update = "UPDATE PROYECTOS SET COD_PR='" + p.getCod_pr() + "'," +
+					"NOMBRE='" + p.getNombre() + "'," +
+					"DES='" + p.getDes() + "' " +
+                    "WHERE COD_PR='" + p.getCod_pr() + "'";
+			//System.out.println(update);
+			
+			st.executeUpdate(update);
+			st.close();
+			return true;
+		}
+		catch(Exception ex) {
+			System.out.println("Error modificando el proyecto - " + ex);
+			return false;
+		}
+	}
+	
+	public void borrarProyecto(String codProyecto) {
+		Statement st;
+		try {
+			st = cn.createStatement();
+			String delete = "DELETE FROM PROYECTOS WHERE COD_PR='"+codProyecto+"'";
+			//System.out.println(delete);
+			
+			st.executeUpdate(delete);
+			st.close();
+		}
+		catch(Exception ex) {
+			System.out.println("Error borrando el proyecto - " + ex);
+		}
+	}
 }
