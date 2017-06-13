c31c43eb904ec525c789c2054e0035e56bfd76b5
Ivan Zhen <ivangpajon@hotmail.com>
2017-06-13 20:30:45 +0200

    Pedidos updated

--- ProyectoIntegrador/src/Entidades/Pedidos.java
+++ ProyectoIntegrador/src/Entidades/Pedidos.java
@@ -1,50 +1,56 @@
 package Entidades;
 
+import java.util.Date;
+
 public class Pedidos {
-
-	private String cod_pd;
-	private String fecha;
-	private int tot_importe;
-	private String cif;
-
-	public Pedidos(String cod_pd, String fecha, int tot_importe, String cif) {
-		super();
+	private int cod_pd;
+	private Date Fecha;
+	private float Tot_importe;
+	private String Cif;
+	
+	public Pedidos(int cod_pd, Date fecha, float tot_importe, String cif) {
 		this.cod_pd = cod_pd;
-		this.fecha = fecha;
-		this.tot_importe = tot_importe;
-		this.cif = cif;
+		this.Fecha = fecha;
+		this.Tot_importe = tot_importe;
+		this.Cif = cif;
 	}
-
-	public String getCod_pd() {
+	
+	public Pedidos() {
+		this.cod_pd = 0;
+		this.Fecha = new Date();
+		this.Tot_importe = 0;
+		this.Cif = "";
+	}
+	
+	public int getCod_pd() {
 		return cod_pd;
 	}
-
-	public void setCod_pd(String cod_pd) {
+	
+	public void setCod_pd(int cod_pd) {
 		this.cod_pd = cod_pd;
 	}
-
-	public String getFecha() {
-		return fecha;
+	
+	public Date getFecha() {
+		return Fecha;
 	}
-
-	public void setFecha(String fecha) {
-		this.fecha = fecha;
+	
+	public void setFecha(Date fecha) {
+		Fecha = fecha;
 	}
-
-	public int getTot_importe() {
-		return tot_importe;
+	
+	public float getTot_importe() {
+		return Tot_importe;
 	}
-
-	public void setTot_importe(int tot_importe) {
-		this.tot_importe = tot_importe;
+	
+	public void setTot_importe(float tot_importe) {
+		Tot_importe = tot_importe;
 	}
-
+	
 	public String getCif() {
-		return cif;
+		return Cif;
 	}
-
+	
 	public void setCif(String cif) {
-		this.cif = cif;
+		Cif = cif;
 	}
-
 }
